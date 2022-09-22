4f0a2471197e3030b02060df435173fe0aa37082
Kiril <kiriarf@gmail.com>
2022-09-22 16:09:21 +0100

    add function to print top n items by column

--- data_processor/data_processor.py
+++ data_processor/data_processor.py
@@ -18,18 +18,38 @@ class DataProcessor():
             reader.fieldnames = helpers.normalise_headers(reader.fieldnames)
 
             for row in reader:
-                row['lease_start_date'] = datetime.strptime(row['lease_start_date'], '%d %b %Y')
-                row['lease_end_date'] = datetime.strptime(row['lease_end_date'], '%d %b %Y')
+                row['lease_start_date'] = datetime.strptime(row['lease_start_date'], '%d %b %Y').date()
+                row['lease_end_date'] = datetime.strptime(row['lease_end_date'], '%d %b %Y').date()
                 row['lease_years'] = int(row['lease_years'])
                 row['current_rent'] = float(row['current_rent'])
                 self.data.append(row)
 
-    # def print_top_n_items_by_column(self, n=5, key='current_rent', data=None):
+    def print_top_n_items_by_column(self, n=5, key='current_rent', data=None):
+        if data is None:
+            data = self.data
+
+        if n > len(data):
+            n = len(data)
+
+        sorted_data = helpers.sort_by_key(key, data)
+
+        print(f'Top {n} masts in ascending order:')
+        for i in range(n):
+            row = sorted_data[i]
+            print(f'{i + 1}.')
+            print(helpers.prettify_row(row))
+
+    # def print_rows_and_total_rent_with_n_lease_years(self, n=25, data=None):
     #     if data is None:
     #         data = self.data
+    #
+    #     rows_with_n_lease_years = [row for row in data if row['lease_years'] == n]
+    #
+    #     total_rent = 0
+    #     for row in rows_with_n_lease_years:
+    #         total_rent += row['current_rent']
+    #         print
 
 
 
-            
 
-        
