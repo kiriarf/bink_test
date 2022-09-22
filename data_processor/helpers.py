db862679a35041860cab8ee4d1b0fc6f5dea7d66
Kiril <kiriarf@gmail.com>
2022-09-22 16:08:21 +0100

    add function to return string for row in printable format and test for it

--- data_processor/helpers.py
+++ data_processor/helpers.py
@@ -4,3 +4,19 @@ def normalise_headers(headers):
 
 def sort_by_key(key, data):
     return sorted(data, key=lambda row: row[key])
+
+
+def prettify_row(row):
+    address_field_values = [row['property_address_1'], row['property_address_2'], row['property_address_3'], row['property_address_4']]
+    full_address = ''
+    for value in address_field_values:
+        if value:
+            full_address += f'{value}, '
+
+    rent_string = f'{row["current_rent"]:.2f} (from {row["lease_start_date"]:%d/%m/%Y} to {row["lease_end_date"]:%d/%m/%Y}, {row["lease_years"]} years)'
+
+    return (f'{"Property: " + row["property_name"] + chr(10) if row["property_name"] else None}'
+          f'{"Address: " + full_address[:-2] + chr(10) if full_address else None}'
+          f'{"Unit: " + row["unit_name"] + chr(10) if row["unit_name"] else None}'
+          f'{"Tenant: " + row["tenant_name"] + chr(10) if row["tenant_name"] else None}'
+          f'{"Rent: " + rent_string + chr(10) if row["current_rent"] else None}')
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import sys
 import unittest
@@ -36,6 +37,43 @@ class TestHelpers(unittest.TestCase):
         assert sorted_data[0]['current_rent'] == min_rent
         assert sorted_data[-1]['current_rent'] == max_rent
 
+    def test_prettify_row(self):
+        row = {
+            'property_name': 'Seacroft Gate (Chase) - Block 2',
+            'property_address_1': 'Telecomms Apparatus',
+            'property_address_2': 'Leeds',
+            'property_address_3': 'Some street',
+            'property_address_4': 'LS14',
+            'unit_name': 'Seacroft Gate (Chase) - Block 2, WYK 0414',
+            'tenant_name': 'Hutchinson3G Uk Ltd&Everything Everywhere Ltd',
+            'lease_start_date': datetime.date(2007, 8, 21),
+            'lease_end_date': datetime.date(2032, 8, 20),
+            'lease_years': 25,
+            'current_rent': 12750.0
+        }
+
+        result = helpers.prettify_row(row)
+
+        assert result == "Property: Seacroft Gate (Chase) - Block 2\nAddress: Telecomms Apparatus, Leeds, Some street, LS14\nUnit: Seacroft Gate (Chase) - Block 2, WYK 0414\nTenant: Hutchinson3G Uk Ltd&Everything Everywhere Ltd\nRent: 12750.00 (from 21/08/2007 to 20/08/2032, 25 years)\n"
+
+    def test_prettify_row_with_incomplete_address(self):
+        row = {
+            'property_name': 'Seacroft Gate (Chase) - Block 2',
+            'property_address_1': 'Telecomms Apparatus',
+            'property_address_2': 'Leeds',
+            'property_address_3': '',
+            'property_address_4': '',
+            'unit_name': 'Seacroft Gate (Chase) - Block 2, WYK 0414',
+            'tenant_name': 'Hutchinson3G Uk Ltd&Everything Everywhere Ltd',
+            'lease_start_date': datetime.date(2007, 8, 21),
+            'lease_end_date': datetime.date(2032, 8, 20),
+            'lease_years': 25,
+            'current_rent': 12750.0
+        }
+
+        result = helpers.prettify_row(row)
+
+        assert result == "Property: Seacroft Gate (Chase) - Block 2\nAddress: Telecomms Apparatus, Leeds\nUnit: Seacroft Gate (Chase) - Block 2, WYK 0414\nTenant: Hutchinson3G Uk Ltd&Everything Everywhere Ltd\nRent: 12750.00 (from 21/08/2007 to 20/08/2032, 25 years)\n"
 
 if __name__ == '__main__':
     unittest.main()
