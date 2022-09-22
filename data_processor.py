de30296eac319df39ef06f3a8dda885c920ebc28
Kiril <kiriarf@gmail.com>
2022-09-22 11:40:46 +0100

    Add skeleton files

---
+++ data_processor.py
@@ -0,0 +1,6 @@
+class DataProcessor():
+    def __init__(file_path):
+        self.file_path = file_path
+
+    def read_from_csv(file_path=self.file_path):
+        pass
---
+++ test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestDataProcessor(unittest.TestCase):
+
+    def test_read_from_csv(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
