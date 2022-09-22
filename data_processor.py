b170be5ec5469a955ba26e85fda2021666464727
Kiril <kiriarf@gmail.com>
2022-09-22 12:08:45 +0100

    add functionality to read rows from csv

--- data_processor.py
+++ data_processor.py
@@ -1,6 +1,20 @@
+import csv
+
 class DataProcessor():
     def __init__(self, file_path):
         self.file_path = file_path
 
     def read_from_csv(self, file_path=None):
-        pass
+        if file_path is None:
+            file_path = self.file_path
+        
+        with open(file_path, mode='r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            
+            result = []
+
+            for row in reader:
+                print(row)
+                result.append(row)
+
+            return result
